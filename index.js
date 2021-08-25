a367aa107a0093aa2db74cdee1689edaff12cd10
David <davidchedrick@gmail.com>
2021-08-24 22:28:40 -0500

    commit tree e3406200aece7d327bb6ee328215c12f829224f3

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(group, name){
+  const driver = group.filter(function (person){return person === name || person === name.toLowerCase() ;});
+  return driver;
+  
+}
+findMatching(drivers, 'Bobby');
+
+
+
+function fuzzyMatch(group, letters){
+  let startWith = group.filter(driver => driver.slice(0,letters.length) === letters);
+  return startWith
+}
+fuzzyMatch(drivers, 'Sa');
+
+
+
+const drivers2 = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+
+function matchName(group, name){
+  const driver = group.filter(function (person){return person.name === name ;});
+    return driver;
+  }
+  matchName(drivers2, 'Bobby');
